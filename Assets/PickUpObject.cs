26873755afc76dc0d815d0d8da3ed97f5159ab31
goh31 <goh@gatech.edu>
2016-10-29 17:03:00 -0400

    Moving object--very glitchy

---
+++ Assets/PickUpObject.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpObject : MonoBehaviour {
+	//boolean to determine whether we are carrying an object
+	bool carrying;
+	GameObject carriedObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//check whether we are carrying an object
+		if (carrying) {
+			//if carrying, carry the carrying object
+			carry (carriedObject);
+		} else {
+			//if not, maybe we want to pickup some object
+			pickup();
+		}
+	}
+
+	void carry(GameObject object) {
+		
+	}
+
+	void pickUp() {
+		//if I press my "E" key, shoot the ray
+		if (Input.GetKeyDown (KeyCode.E)) {
+			//Determine middle of screen
+			int x = Screen.width / 2;
+			int y = Screen.height / 2;
+
+			Ray ray = Camera.main.ScreenPointToRay (new Vector3 (x, y));
+
+			RaycastHit raycastHit;
+			if(Physics.Raycast(ray, out raycastHit)) {
+				Pickable pick = raycastHit.collider.GetComponent<Pickable> ();
+				//exists, we're allowed to pick up the object
+				if (pick != null) {
+					carrying = true;
+					carriedObject = pick.gameObject;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/Pickup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Pickup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	GameObject getMouseHoverObject (float range) {
+		//vector3 = where we are
+		Vector3 position = gameObject.transform.position;
+		RaycastHit raycastHit;
+		Vector3 target = position + Camera.main.transform.forward * range;
+		//if there is a collision
+		if (Physics.Linecast (position, target, out raycastHit))
+			return raycastHit.collider.gameObject;
+		return null;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (getMouseHoverObject (5));
+	}
+}
